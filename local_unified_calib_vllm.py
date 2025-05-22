ec5d3075e9bc7f28a70c1affaf5d69b04572548d
Techie5879 <aritrahtml@gmail.com>
2025-05-22 05:52:39 +0000

    many qwen 3b

--- local_unified_calib_vllm.py
+++ local_unified_calib_vllm.py
@@ -465,7 +465,7 @@ def save_experiment_details(args, safe_model_name, base_dir, model_name, model_i
 if __name__ == "__main__":
     import argparse
     '''
-    vllm serve meta-llama/Meta-Llama-3.1-8B-Instruct   --max-model-len 4096   --dtype bfloat16   --gpu-memory-utilization 0.94   --trust-remote-code --max-logprobs 25
+    vllm serve Qwen/Qwen2.5-3B-Instruct --max-model-len 4096   --dtype bfloat16   --gpu-memory-utilization 0.94   --trust-remote-code --max-logprobs 25
     '''
     parser = argparse.ArgumentParser()
     parser.add_argument(
